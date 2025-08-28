bb9c619c7c8cce8624e00dd0a0e2d5e2095c77ba
vavlxxx <vladimir.nefedov357@gmail.com>
2025-08-28 09:15:48 +0500

    new: main add is ready

---
+++ app/main/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "title": "Home | Главная",
+        "content": "Магазин мебели HOME",
+    }
+    return render(request, "main/index.html", context)
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    context = {
+        "title": "Home | О нас",
+        "content": "Магазин мебели HOME",
+    }
+    return render(request, "main/about.html", context)
